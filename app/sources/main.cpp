6bf01e561a7afb633ca0e87a4f4404b2c955f2a5
Mishka Rogachev <mishkarogachev@gmail.com>
2014-05-13 01:46:26 +0400

    Template qml app added

--- app/sources/main.cpp
+++ app/sources/main.cpp
@@ -1,6 +1,13 @@
+#include <QtGui/QGuiApplication>
+#include <QQuickView>
 
-
-int main()
+int main(int argc, char *argv[])
 {
-    return 0;
+    QGuiApplication app(argc, argv);
+    QQuickView view;
+    view.setSource(QUrl("qrc:/qml/Main.qml"));
+    view.setResizeMode(QQuickView::SizeRootObjectToView);
+    view.setMinimumSize(QSize(1280, 800));
+    view.show();
+    return app.exec();
 }
