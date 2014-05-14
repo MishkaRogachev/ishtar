95cf01d6041c01b86d930fd9a1d379a1f1322fbd
Mishka Rogachev <mishkarogachev@gmail.com>
2014-05-14 13:43:29 +0400

    BondingBox basic implementation

--- core/sources/situation/bounding_box.cpp
+++ core/sources/situation/bounding_box.cpp
@@ -4,3 +4,24 @@ using namespace situation;
 
 BoundingBox::BoundingBox()
 {}
+
+QVector3D BoundingBox::min() const
+{
+    return m_min;
+}
+
+QVector3D& BoundingBox::rMin()
+{
+    return m_min;
+}
+
+QVector3D BoundingBox::max() const
+{
+    return m_max;
+}
+
+QVector3D& BoundingBox::rMax()
+{
+    return m_max;
+}
+
--- core/sources/situation/bounding_box.h
+++ core/sources/situation/bounding_box.h
@@ -1,12 +1,24 @@
 #ifndef BOUNDING_BOX_H
 #define BOUNDING_BOX_H
 
+#include <QVector3D>
+
 namespace situation
 {
     class BoundingBox
     {
     public:
         BoundingBox();
+
+        QVector3D min() const;
+        QVector3D& rMin();
+
+        QVector3D max() const;
+        QVector3D& rMax();
+
+    private:
+        QVector3D m_min;
+        QVector3D m_max;
     };
 }
 
